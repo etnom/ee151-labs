d0d2f5a63e6fab3618797d7630cc5a2f234c9057
etnom <montgomery.okdaily@gmail.com>
2018-10-16 13:57:02 -0700

    funciton for left motor power

--- Week 4/robotMotorSpeed/robotMotorSpeed.ino
+++ Week 4/robotMotorSpeed/robotMotorSpeed.ino
@@ -26,3 +26,17 @@ void rightMotor(int speed) {
 	analogWrite(IN1, abs(speed));
 
 }
+
+//neg speed = backwards
+void leftMotor(int speed) {
+	if (speed > 0) {
+		digitalWrite(IN3, LOW);
+		analogWrite(IN4, abs(speed));
+
+		return;
+	}
+
+	digitalWrite(IN4, LOW);
+	analogWrite(IN3, abs(speed));
+
+}
