7ebbb76f39b890dc0d3e51d9460b34e3c9348396
Jack Meyers <43621292+JackMeiers@users.noreply.github.com>
2018-10-23 11:41:09 -0700

    Update pivotAndCircle.ino

--- Week 4/pivotAndCircle/pivotAndCircle.ino
+++ Week 4/pivotAndCircle/pivotAndCircle.ino
@@ -1,8 +1,12 @@
+// EE 151 - 02 Pivot and Circle sketch
+// Jackson Meyers, Monty Choy
+
 #define IN1 4
 #define IN2 5
 #define IN3 6
 #define IN4 7
 
+// setup pins
 void setup() {
   for (int i = 4; i < 8; i++) {
     pinMode(i, OUTPUT);
@@ -11,23 +15,31 @@ void setup() {
 
 void loop() 
 {
+  // Move backwards for one sec
   rightMotor(-255);
   leftMotor(-255);
   delay(1000);
+  // Halt
   stopAllMotors();
   delay(1000);
+  // Pivot and stop
   rightMotor(255);
   leftMotor(-255);
   delay(250);
   stopAllMotors();
 
+  // Wait
   delay(100);
 
+  // Move in a circle
   leftMotor(50);
   rightMotor(150);
+  // Do this for 5.29 seconds to complete the proper revolutions
   delay(5290);
+  // Stop
   stopAllMotors();
 
+  // Wait for 3 seconds before the loop restarts
   delay(3000);
 
 }
