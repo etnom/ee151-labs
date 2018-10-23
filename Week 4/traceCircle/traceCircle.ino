4fd80c9a57ebb4b166eb820bf39c274082f62892
Jack Meyers <43621292+JackMeiers@users.noreply.github.com>
2018-10-23 11:37:13 -0700

    Update traceCircle.ino

--- Week 4/traceCircle/traceCircle.ino
+++ Week 4/traceCircle/traceCircle.ino
@@ -1,8 +1,12 @@
+// EE151 - 02 Circle sketch
+// Jackson Meyers, Monty Choy
+
 #define IN1 4
 #define IN2 5
 #define IN3 6
 #define IN4 7
 
+// setup pins
 void setup() {
 	for (int i = 4; i < 8; i++) {
 		pinMode(i, OUTPUT);
@@ -10,9 +14,12 @@ void setup() {
 }
 
 void loop() {
+	// Move right faster than left to turn in a circle
 	leftMotor(50);
 	rightMotor(150);
+	// do this for 5.29 seconds
 	delay(5290);
+	// stop
 	stopAllMotors();
 }
 
