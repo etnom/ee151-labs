e4f82ce3e30e47b85248203258b6841d967f9b07
etnom <montgomery.okdaily@gmail.com>
2018-10-23 14:42:15 -0700

    standarize formatiing

--- Week 5/move/move.ino
+++ Week 5/move/move.ino
@@ -1,23 +1,24 @@
 void move(int leftSpeed, int rightSpeed){
 	int leftSpeedPWM = leftSpeed*2.55;
 	int rightSpeedPWM = rightSpeed*2.55;
+	
 	//turn both motors off
 	digitalWrite(L1, LOW);
 	digitalWrite(L2, LOW);
 	digitalWrite(R1, LOW);
 	digitalWrite(R2, LOW);
+
 	//make left motor move
-	if(leftSpeedPWM < 0){
+	if (leftSpeedPWM < 0) {
 		analogWrite(L1, abs(leftSpeedPWM));
-	}
-	else{
+	} else {
 		analogWrite(L2, leftSpeedPWM);
 	}
+
 	//make right motor move
-	if(rightSpeedPWM < 0){
+	if (rightSpeedPWM < 0) {
 		analogWrite(R1, abs(rightSpeedPWM));
-	}
-	else{
+	} else {
 		analogWrite(R2, rightSpeedPWM);
 	}
 }
