3130cb30de335b98b2e5f9b243ee4bfc6b341852
etnom <montgomery.okdaily@gmail.com>
2018-11-13 13:21:17 -0800

    buzzer and led action based on distance

--- Week 8/ObstacleAvoider/ObstacleAvoider.ino
+++ Week 8/ObstacleAvoider/ObstacleAvoider.ino
@@ -22,26 +22,41 @@ void setup() {
 
 
 void loop() {
+	pulseUltrasonicTrigger();	
+
+	double pulseWidth = pulseIn(ECHO_PIN, HIGH);
+	float distance = calculateDistanceFromPulseWidth(pulseWidth);
+
+	Serial.println("Pulse width: " + (String)pulseWidth);
+	Serial.println("Distance: " + (String)distance);
+
+	if (distance > 6) {
+		digitalWrite(LED_PIN, HIGH);
+	} else {
+		digitalWrite(LED_PIN, LOW);
+	}
+
+	if (distance > 20) {
+    	tone(BUZZER_PIN, map(distance, 0, 20, 2000, 100));
+	}
+
+	delay(500);
+}
+
+void pulseUltrasonicTrigger() {
 	digitalWrite(TRIG_PIN, LOW);
 	delayMicroseconds(2);
 	digitalWrite(TRIG_PIN, HIGH);
 	delayMicroseconds(10);
 	digitalWrite(TRIG_PIN, LOW);
-
-	unsigned long pulseWidth = pulseIn(ECHO_PIN, HIGH);
-	float distance = calculateDistanceFromPulseWidth(pulseWidth);
-
-	Serial.println("DIstance: " + (String)distance);
-
-	delay(500);
 }
 
 //calculate distance based on pulse width
 double calculateDistanceFromPulseWidth(double pulseWidth) {
   if (pulseWidth < 300){
     return 0;
-  } else {
-    return pulseWidth / 144;
-  }
+  } 
+
+	return pulseWidth / 74 / 2;
 
 }
