c90dbbf1d92ea6d74f6607661d1109e8e61bf7e2
etnom <montgomery.okdaily@gmail.com>
2018-11-12 18:23:13 -0800

    functions to init and disable motor pins

--- Week 8/Prelab/ultrasonicSensor/ultrasonicSensor.ino
+++ Week 8/Prelab/ultrasonicSensor/ultrasonicSensor.ino
@@ -3,9 +3,15 @@
 // Week 8 Prelab
 // Test and verify operation of ultrasonic sensor
 
+//pins for motors
+#define IN1 4
+#define IN2 5
+#define IN3 6
+#define IN4 7
 
 void setup() {
-
+	initMotorPins();
+	disableMotors();
 }
 
 void loop() {
@@ -13,3 +19,16 @@ void loop() {
 }
 
 
+//function to set pinModes of motor pins
+void initMotorPins() {
+	for (int i = 4; i < 8; i++) {
+    pinMode(i, OUTPUT);		//set all motor pins as output
+  }
+}
+
+//function to disable motor pins
+void disableMotors() {
+	for (int i = 4; i < 8; i++) {
+    digitalWrite(i, LOW);		//set all pins to low
+  }
+}
