34d67c70a56e0f1354c9e795d65b38be48be1341
etnom <montgomery.okdaily@gmail.com>
2018-11-26 22:32:15 -0800

    add file and pins for lineFollowerSensorArrayMonitor

---
+++ Week 9/lineFollowerSensorArrayMonitor/lineFollowerSensorArrayMonitor.ino
@@ -0,0 +1,23 @@
+// Monty Choy 
+// EE 151-02
+// Mon - 11/26/18
+// Reads all 5 line sensors and returns a single digital code word that indicates 
+// if each sensor is over a black or white surface
+
+//pins for line sensors
+#define FAR_LEFT_SENSOR_PIN 15
+#define LEFT_SENSOR_PIN 14 
+#define MIDDLE_SENSOR_PIN 13
+#define RIGHT_SENSOR_PIN 12
+#define FAR_RIGHT_SENSOR_PIN 11
+ 
+
+void setup() {
+	Serial.begin(9600);					//begin serial at 9600 baud rate
+
+}
+
+void loop() {
+
+}
+
